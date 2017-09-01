c06b7fb80566e484528414d77310c23aa281fc8b
Gabriel Saruhashi <gabrielsaruhashi@Gabriels-MacBook-Pro.local>
2017-09-01 11:47:18 -0400

    // output working

--- pset1/comments.c
+++ pset1/comments.c
@@ -5,15 +5,42 @@ all subsequent steps to someone else). */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
+#define CHUNK 1024 /* read 1024 bytes at a time */
+
+int getIndexOfContentStart(char line[CHUNK], int type) {
+	// for type, 0 = "//" comment, and 1 == "/*" comment
+	if (type == 0) {
+		// strlen - 1 because I'm accessing i + 1
+		for (int i = 0; i < strlen(line) - 1; i++) {
+			// find index of either "//" or "/*"
+			if (line[i] == '/' &&  
+				line[i + 1] == '/') {
+
+				int index = i + 2;
+				while(line[index] == ' ') {
+					index += 1;
+				}
+
+				return index;
+			}
+		}
+		//TODO return something that indicate it was not found
+		return CHUNK;
+	} else if (type == 1) {
+		return CHUNK;
+	}
+		
+}
 
 int main(int argc, char **argv) {
 	// declare variables
 	FILE *file;
 	// location to store the bytes that get read from the file
 	// where to put the bytes after it's read them
-	//TODO find out what should be buffer size
-	#define CHUNK 1024 /* read 1024 bytes at a time */
+	
 	char line[CHUNK];
 
 
@@ -40,12 +67,30 @@ int main(int argc, char **argv) {
  	while (fgets(line, sizeof(line), file)) {
         /* note that fgets don't strip the terminating \n, checking its
            presence would allow to handle lines longer that sizeof(line) */
-        
-        if (ferror(file)) {
-        	/* deal with error */
-        	printf("Error reading");
-    	}
-        printf("%s", line); 
+
+ 		// check if it is a comment
+ 		if (strstr(line, "//")) {
+
+ 			// check if it is contained in a string or character literal
+ 			// type 0 is "//"
+ 			int indexStart = getIndexOfContentStart(line, 0);
+
+ 			char* output = malloc(strlen(line) - (indexStart + 1));
+
+ 			// char* strncpy(char* dest, char* src, int size)
+ 			// indexStart. + 1 because index starts at 0
+ 			output = strncpy(output, line + indexStart, strlen(line) - (indexStart + 1));
+
+ 			// add the terminating \n
+ 			output[strlen(output)] = '\n';
+
+ 			// strip comments
+        	printf("%s", output); 
+ 		
+
+ 		}
+
+ 		
     }
 	// close line
 	 fclose(file);
