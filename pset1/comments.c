b8c28aada22a3a266e9b6035128d0cb9594d77bc
Gabriel Saruhashi <gabrielsaruhashi@Gabriels-MacBook-Pro.local>
2017-09-03 00:09:58 -0400

    // case solved

--- pset1/comments.c
+++ pset1/comments.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdbool.h>
+
+void removeTag() {
+	int ch;
+
+	// handles the case when it is an isolated tag
+	if ((ch = getchar()) == ' ') {
+		putchar('@');
+		putchar(ch);
+
+	} else {
+		while ((ch = getchar()) != EOF &&
+			ch != ' ') {
+			// do nothing
+		}
+
+		// put back space
+		if (ch == ' ') {
+			ungetc(ch, stdin);
+		}
+	}
+
+}
 
 /* ignores all trailing spaces and asterisks*/
 void removeTrailingStuff() {
@@ -8,24 +31,21 @@ void removeTrailingStuff() {
 	while ((ch = getchar()) != EOF) {
 		if (ch != ' ' &&
 			ch != '*') {
+			// check if char is not a tag
+			if (ch == '@') {
+				removeTag();
+				continue;
+
+			}
 			// put last character back to be read by main function
 			ungetc(ch, stdin);
-			// break if
+		
+			
+			// break loop
 			break;
 		}
-	}
-}
 
-void removeTag() {
-	int ch;
-
-	while ((ch = getchar()) != EOF &&
-		ch != ' ') {
-		// do nothing
-	}
-	// put back space
-	if (ch == ' ') {
-		ungetc(ch, stdin);
+	
 	}
 }
 
@@ -41,8 +61,8 @@ int main()
 {
   int ch;
   int nextChar;
-  int commuteChar;
   int supportCommuteChar;
+  int commuteChar;
 
   while ((ch = getchar()) != EOF)
     {
@@ -58,6 +78,14 @@ int main()
     		if ((nextChar = getchar()) == '/') {
     			// remove trailing spaces and *
     			removeTrailingStuff();
+    			// handle case when comment is empty OR only with leading spaces
+    			if ((supportCommuteChar = getchar()) == EOF ||
+    				supportCommuteChar == '\n') {
+    				continue;
+    			} else {
+    				ungetc(supportCommuteChar, stdin);
+    			}
+
     			// write to stout until it reaches the end of commented line
     			while ((commuteChar = getchar()) != EOF) {
     				if (commuteChar != '\n' &&
@@ -65,13 +93,7 @@ int main()
     					putchar(commuteChar);
     				} else if (commuteChar == '@') {
     					// check if next char is a word char, which indicates it is a tag
-    					if ((supportCommuteChar = getchar()) != EOF &&
-    						supportCommuteChar != ' ') {
-    						removeTag();
-    					// handles case when there is an isolated '@' in a comment
-    					} else if (supportCommuteChar == ' ') {
-    						putchar(supportCommuteChar);
-    					}
+    					removeTag();
     				} else { // if it reaches end of comment, break the while loop
     					putchar('\n');
     					break;
@@ -79,6 +101,7 @@ int main()
     				
     			}
     		} else if (nextChar == '*') {
+    			// remove trailing stuff
     			removeTrailingStuff();
     		}
     	}
