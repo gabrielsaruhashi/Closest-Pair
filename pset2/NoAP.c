2b48f619b48e0eb3ac05077cef231c9ae7c50755
Gabriel Saruhashi <gabrielsaruhashi@Gabriels-MBP.wireless.yale.internal>
2017-09-21 10:49:49 -0400

    Formatted backward and greedy:

--- pset2/NoAP.c
+++ pset2/NoAP.c
@@ -3,6 +3,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+bool isNumber(char number[])
+{
+    int i = 0;
+
+    //checking for negative numbers
+    if (number[0] == '-')
+        i = 1;
+    for (; number[i] != 0; i++)
+    {
+        //if (number[i] > '9' || number[i] < '0')
+        if (!isdigit(number[i]))
+            return false;
+    }
+    return true;
+}
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)      
+ 
+       // Last i elements are already in place   
+       for (j = 0; j < n-i-1; j++) 
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+}
+ 
 
 /* Function to discover whether integers x,y,z form a AP in a set
 x > y so options for an AP are xyz, zxy, xzy */
@@ -55,6 +92,9 @@ int main(int argc, char *argv[])
 	int sizeOfMustHaveIntegers = 0;
 	int largestMustHaveInt = 0;
 
+	char *methods[4];
+	int numberOfMethods = 0;
+
 	 // helper variables to go through all the arguments
 	int argcHelper = 0;
 	bool isMustHaveInteger = true;
@@ -76,7 +116,9 @@ int main(int argc, char *argv[])
 	    }
 
 	    // add all must-have integers to the array
-	    if (argcHelper > 1 && isMustHaveInteger == true) {
+	    //if (isdigit(atoi(argv[argcHelper])))
+	    if (argcHelper > 1 && isNumber(argv[argcHelper]))
+	    {
 	    	int newMustHaveInt = atoi(argv[argcHelper]);
 	    	mustHaveIntegers[sizeOfMustHaveIntegers] = newMustHaveInt ;
 
@@ -85,34 +127,49 @@ int main(int argc, char *argv[])
 	    		largestMustHaveInt = newMustHaveInt;
 	    	}
 	    	sizeOfMustHaveIntegers++; 
+	    } else if ((argcHelper > 1 && argv[argcHelper][0] == '-')) 
+	    // add method to method array
+	    {
+	    	methods[numberOfMethods] = argv[argcHelper];
+	    	numberOfMethods++;
 	    }
 
 	    argcHelper++;
 	} while (argcHelper < argc);
-   
 
     int array[range];
 	int sizeOfArray = 0;
-	bool allValidMustHaveIntegers;
+	int backwardArray[range];
+	int sizeOfBackwardArray = 0;
+
+	int hasInvalidMustHaveIntegers = false;
 
 
 
 	// go through must have integer array to make sure there are no PAs
-	for (int i = 0; i < sizeOfMustHaveIntegers; i++) {
+	for (int i = 0; i < sizeOfMustHaveIntegers; i++) 
+	{
 
 		// just add the first two numbers
 		if (sizeOfArray < 2) 
 		{ 
 			array[sizeOfArray] = mustHaveIntegers[i];
+			backwardArray[sizeOfBackwardArray] = mustHaveIntegers[i];
 			// keep array size updated
 			sizeOfArray += 1;
+			sizeOfBackwardArray += 1;
 		} else {
 			// iterate through the current array to see if any pair (x, y) makes AP with z 
 			if (!anyAPViolation(mustHaveIntegers[i], sizeOfArray, array)) {
-				array[sizeOfArray] = mustHaveIntegers[i];
-				sizeOfArray += 1;
+					array[sizeOfArray] = mustHaveIntegers[i];
+					backwardArray[sizeOfBackwardArray] = mustHaveIntegers[i];
+					// keep array size updated
+					sizeOfArray += 1;
+					sizeOfBackwardArray += 1;
+
+
 			} else {
-				allValidMustHaveIntegers = false;
+				hasInvalidMustHaveIntegers = true;
 				printf("0 []\n");
 				break;
 			}
@@ -120,45 +177,92 @@ int main(int argc, char *argv[])
 
 	}
 
-    /* GREEDY */
+   
+   for (int i = 0; i < numberOfMethods; i++) {
 
-    // initialize prospective member according to whether or not there is 
-    int prospectiveMember = largestMustHaveInt + 1;
+   		if (strcmp(methods[i], "-greedy") == 0) {
+   			// initialize prospective member according to whether or not there is 
+		    int prospectiveMember = largestMustHaveInt + 1;
 
-    // iterate through the set of numbers, starting with the largest must-have int
-    for (; prospectiveMember < range; prospectiveMember++) {
-    	
-		// just add the first two numbers
-		if (sizeOfArray < 2) 
-		{ 
-			array[sizeOfArray] = prospectiveMember;
-			// keep array size updated
-			sizeOfArray += 1;
-		} 
-		else 
-		{
-			// iterate through the current array to see if any pair (x, y) makes AP with z 
-			if (!anyAPViolation(prospectiveMember, sizeOfArray, array)) {
-				array[sizeOfArray] = prospectiveMember;
-				sizeOfArray += 1;
+		     /*********** GREEDY ***********/
+		    // iterate through the set of numbers, starting with the largest must-have int
+		    for (; prospectiveMember < range; prospectiveMember++) {
+		    	
+				// just add the first two numbers
+				if (sizeOfArray < 2) 
+				{ 
+					array[sizeOfArray] = prospectiveMember;
+					// keep array size updated
+					sizeOfArray += 1;
+				} 
+				else 
+				{
+					// iterate through the current array to see if any pair (x, y) makes AP with z 
+					if (!anyAPViolation(prospectiveMember, sizeOfArray, array)) {
+						array[sizeOfArray] = prospectiveMember;
+						sizeOfArray += 1;
+					}
+				}
 			}
-		}
-	}
 
-	// output array of integers 
-	printf("[");
-	for (int i = 0; i < sizeOfArray; i ++) {
-		printf("%i", array[i]);
+			// output array of integers 
+			printf("-greedy: %i [", sizeOfArray);
+			for (int i = 0; i < sizeOfArray; i ++) {
+				printf("%i", array[i]);
 
-		if (i < sizeOfArray - 1) {
-			printf("%c", ',');
-		} else {
-			printf("]\n");
-		}
-		
-	}
+				if (i < sizeOfArray - 1) {
+					printf("%c", ',');
+				} else {
+					printf("]\n");
+				}
+				
+			}
+   		} else if (strcmp(methods[i], "-backward") == 0) {
+   			/*********** BACKWARD ***********/
+
+			int prospectiveBackWardMember = range - 1;
+			for (; prospectiveBackWardMember > largestMustHaveInt; prospectiveBackWardMember--) {
+
+				// just add the first two numbers
+				if (sizeOfBackwardArray < 2) 
+				{ 
+					backwardArray[sizeOfBackwardArray] = prospectiveBackWardMember;
+					// keep array size updated
+					sizeOfBackwardArray += 1;
+				} 
+				else 
+				{
+					// iterate through the current array to see if any pair (x, y) makes AP with z 
+					if (!anyAPViolation(prospectiveBackWardMember, sizeOfBackwardArray, backwardArray)) {
+						backwardArray[sizeOfBackwardArray] = prospectiveBackWardMember;
+						sizeOfBackwardArray += 1;
+					}
+				}
+			}
+
+			// sortprospective backward memeber
+			bubbleSort(backwardArray, sizeOfBackwardArray);
 
 
+			// output array of integers 
+			printf("-backward: %i [", sizeOfBackwardArray);
+			for (int i = 0; i < sizeOfBackwardArray; i ++) {
+				printf("%i", backwardArray[i]);
+
+				if (i < sizeOfBackwardArray - 1) {
+					printf("%c", ',');
+				} else {
+					printf("]\n");
+				}
+				
+			}
+
+   		}
+   	
+
+   }
+
+    
 	// format string 
 		
 
---
+++ pset2/sort.h
@@ -0,0 +1,10 @@
+
+/**
+ * Function to facilitate swapping
+ * numbers around while sorting.
+ * 
+ */
+void swap(int *xp, int *yp);
+
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n);
